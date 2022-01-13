3d7151a7a27c4b0fc418eb08e2690143e4a7ce00
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-13 10:45:33 +0300

    Asp.Net Core Mvc projesi oluşturma-4

---
+++ RealWorldUnitTest.Web/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RealWorldUnitTest.Web.Models;
+
+namespace RealWorldUnitTest.Web.Repository
+{
+    public class Repository<TEntity>:IRepository<TEntity> where TEntity :class
+    {
+        private readonly UdemyUnitTestDBContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+        public Repository(UdemyUnitTestDBContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+             return await _dbSet.FindAsync(id);
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+            _dbSet.Update(entity);
+
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
